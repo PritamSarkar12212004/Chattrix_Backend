f76591d4f95a0bb0b9fd3f791ba3bebda3d61c71
Pritam Sarkar <pritam7796419792@gmail.com>
2025-03-21 01:49:34 +0530

    messag reive done send done all

---
+++ src/sockets/event/chats/status/userStatusChaker/userSattusChaker.js
@@ -0,0 +1,17 @@
+import user_key_map from "../../../map/keyMap.js";
+
+const userSattusChaker = (io, socket) => {
+  socket.on("user-status-chaker", async (payload) => {
+    const { chekerRequestid, payloadId } = payload;
+    // Get sender & receiver socket IDs
+    const senderId = await user_key_map.get(chekerRequestid);
+    const receiverId = await user_key_map.get(payloadId);
+
+    if (payload && user_key_map.has(payloadId)) {
+      io.to(senderId).emit("get-user-status", "online");
+    } else {
+      io.to(senderId).emit("get-user-status", "offline");
+    }
+  });
+};
+export default userSattusChaker;
--- src/sockets/manager/socketManager.js
+++ src/sockets/manager/socketManager.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import registerSocket from "../event/chats/auth/registerSocket.js";
 import sendTextMessage from "../event/chats/text/sender/sendTextMessage.js";
+import userSattusChaker from "../event/chats/status/userStatusChaker/userSattusChaker.js";
 const socketManager = (server) => {
   const io = new Server(server, {
     cors: {
@@ -12,6 +13,8 @@ const socketManager = (server) => {
   io.on("connection", (socket) => {
     // register the socket
     registerSocket(io, socket);
+    // user status checker
+    userSattusChaker(io, socket);
 
     // text sending
     sendTextMessage(io, socket);

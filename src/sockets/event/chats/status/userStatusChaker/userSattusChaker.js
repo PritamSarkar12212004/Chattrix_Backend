22fb506282c58fd6f050bc9122472e18bead425a
Pritam Sarkar <pritam7796419792@gmail.com>
2025-03-21 01:53:29 +0530

    done userSattus chaker

--- src/sockets/event/chats/status/userStatusChaker/userSattusChaker.js
+++ src/sockets/event/chats/status/userStatusChaker/userSattusChaker.js
@@ -5,7 +5,6 @@ const userSattusChaker = (io, socket) => {
     const { chekerRequestid, payloadId } = payload;
     // Get sender & receiver socket IDs
     const senderId = await user_key_map.get(chekerRequestid);
-    const receiverId = await user_key_map.get(payloadId);
 
     if (payload && user_key_map.has(payloadId)) {
       io.to(senderId).emit("get-user-status", "online");
